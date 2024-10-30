47c3eda63adb143c295fc73c6c538e92006b8cf9
jiyeong <tokyru@naver.com>
2024-10-30 16:12:47 +0900

    upload

---
+++ src/components/HeaderRight.js
@@ -0,0 +1,23 @@
+import { Pressable } from 'react-native';
+import PropTypes from 'prop-types';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { GRAY, PRIMARY } from '../colors';
+
+const HeaderRight = ({ onPress, disabled = false }) => {
+  return (
+    <Pressable onPress={onPress} disabled={disabled} hitSlop={10}>
+      <MaterialCommunityIcons
+        name="check"
+        size={24}
+        color={disabled ? GRAY.DEFAULT : PRIMARY.DEFAULT}
+      />
+    </Pressable>
+  );
+};
+
+HeaderRight.propTypes = {
+  onPress: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+export default HeaderRight;
